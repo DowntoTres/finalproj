d8195e0792229f9600b0c7ede1b94e9328431e03
yparks <Mills2016>
2017-05-01 22:19:47 -0700

    DB changes

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     function ($scope, Authentication) {
-        // This provides Authentication context.
+// This provides Authentication context.
         $scope.authentication = Authentication;
 
         $scope.alerts = [
